a2f8a3ef7e4e172e571685b419f7fdb8d902b224
ArielFernando24 <arielfernandoalves@hotmail.com>
2024-06-26 17:08:20 -0300

    First Commit

---
+++ teste/src/main/java/com/teste/classes/Funcionario.java
@@ -0,0 +1,31 @@
+package com.teste.classes;
+
+import java.math.*;
+import java.time.*;
+
+public class Funcionario extends Pessoa {
+    private BigDecimal salario;
+    private String funcao;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
